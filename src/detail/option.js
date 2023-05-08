7d70df3156fd23af713cbb424219457e6d4e74e3
“Quang <thongquang.nguyen@dxc.com>
2023-05-08 21:36:01 +0700

    Ticket-1: Test commit code after comback

--- src/detail/option.js
+++ src/detail/option.js
@@ -2,7 +2,6 @@ import React,{Component} from "react";
 
 class Option extends Component{
     render(){
-        console.log(this.props)
 return (
     <>
     <option value={this.props.id}>{this.props.name}</option>
